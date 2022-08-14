bfdbde5908ef1bff801232bbbbac22136cfde9e0
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2022-08-14 19:39:36 +0300

    added splash logo with lottie

--- lib/constants/app_theme.dart
+++ lib/constants/app_theme.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
-final darkBlue = Color(0xFF1C2151);
-final linearColorOne = Color(0xFF1A1F4E);
-final linearColorTwo = Color(0xFF23295E);
-final purple = Color(0xFF624DBE);
+const darkBlue = Color(0xFF1C2151);
+const linearColorOne = Color(0xFF1A1F4E);
+const linearColorTwo = Color(0xFF23295E);
+const purple = Color(0xFF624DBE);
 
 class AppTheme {
   final darkTheme = ThemeData(
--- lib/view/splash_view.dart
+++ lib/view/splash_view.dart
@@ -1,23 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:lottie/lottie.dart';
 import 'package:tictactoe_multiplayer_game/constants/app_assets.dart';
 import 'package:tictactoe_multiplayer_game/constants/app_theme.dart';
+import 'package:tictactoe_multiplayer_game/view/home_view.dart';
 
-class SplashView extends StatelessWidget {
+class SplashView extends StatefulWidget {
   const SplashView({Key? key}) : super(key: key);
+
+  @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> with TickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(seconds: (1)),
+      vsync: this,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        decoration: BoxDecoration(
+        alignment: Alignment.center,
+        decoration: const BoxDecoration(
           gradient: LinearGradient(
               begin: Alignment.topCenter,
               end: Alignment.bottomCenter,
               colors: [linearColorOne, linearColorTwo]),
-          image: const DecorationImage(
+          image: DecorationImage(
             image: AssetImage(AppAsset.backgroundImage),
             fit: BoxFit.fill,
           ),
+        ),
+        child: Lottie.asset(
+          AppAsset.splashLogoGif,
+          width: 173.w,
+          height: 173.h,
+          controller: _controller,
+          onLoaded: (composition) {
+            _controller
+              ..duration = composition.duration
+              ..forward().whenComplete(
+                () => Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => const HomeView()),
+                ),
+              );
+          },
         ),
       ),
     );

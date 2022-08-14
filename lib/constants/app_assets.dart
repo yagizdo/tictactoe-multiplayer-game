dd173282f8581f1c2e625d7cff42ad7120120241
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2022-08-14 23:37:30 +0300

    splash logo updated

--- lib/constants/app_assets.dart
+++ lib/constants/app_assets.dart
@@ -1,4 +1,4 @@
 class AppAsset {
   static const String backgroundImage = 'lib/asset/img/background.png';
-  static const String splashLogoGif = 'lib/asset/img/coloredtictactoe.json';
+  static const String splashLogoGif = 'lib/asset/img/splashlogo.json';
 }

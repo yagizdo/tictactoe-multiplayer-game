d313ddc93f9b51b31e6f2e6eecb50e8cb94da894
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2022-08-14 19:14:50 +0300

    added splash view and splash background asset

---
+++ lib/view/splash_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SplashView extends StatelessWidget {
+  const SplashView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}

b8c43a7220bacbddbc356347d3c06421329cf5a0
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2022-08-16 00:07:04 +0300

    added app_constants

---
+++ lib/constants/app_constants.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+SizedBox height25 = SizedBox(
+  height: 25.h,
+);
+SizedBox height30 = SizedBox(
+  height: 30.h,
+);
+
+SizedBox height40 = SizedBox(
+  height: 40.h,
+);
+
+SizedBox height50 = SizedBox(
+  height: 50.h,
+);
+
+SizedBox height20 = SizedBox(
+  height: 20.h,
+);
+
+SizedBox height150 = SizedBox(
+  height: 150.h,
+);
+
+SizedBox height8 = SizedBox(
+  height: 8.h,
+);
+
+SizedBox height67 = SizedBox(
+  height: 67.h,
+);

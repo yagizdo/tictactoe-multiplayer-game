65df7cc543fc8443b0b291835f5dbb80f50ae093
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2022-08-15 22:44:28 +0300

    internet manager => Network Manager

--- lib/core/i_network_manager.dart
+++ lib/core/i_network_manager.dart
@@ -2,7 +2,7 @@ import 'package:tictactoe_multiplayer_game/core/network_result.dart';
 
 typedef NetworkCallback = void Function(NetworkResult result);
 
-abstract class IInternetManager {
+abstract class INetworkManager {
   void handleNetworkChange(NetworkCallback onChange);
   void dispose();
 }
--- lib/core/network_manager.dart
+++ lib/core/network_manager.dart
@@ -1,14 +1,14 @@
 import 'dart:async';
 
 import 'package:internet_connection_checker/internet_connection_checker.dart';
-import 'package:tictactoe_multiplayer_game/core/i_internet_manager.dart';
+import 'package:tictactoe_multiplayer_game/core/i_network_manager.dart';
 import 'package:tictactoe_multiplayer_game/core/network_result.dart';
 
-class InternetManager extends IInternetManager {
+class NetworkManager extends INetworkManager {
   late final InternetConnectionChecker _connectionChecker;
   StreamSubscription<InternetConnectionStatus>? _subscription;
 
-  InternetManager() {
+  NetworkManager() {
     _connectionChecker = InternetConnectionChecker.createInstance(
       checkInterval: const Duration(seconds: 3),
     );

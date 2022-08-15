975acd11e6660b53989f5b112f7ed80bc6e8781d
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2022-08-15 22:41:48 +0300

    added internet manager

---
+++ lib/core/internet_manager.dart
@@ -0,0 +1,31 @@
+import 'dart:async';
+
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:tictactoe_multiplayer_game/core/i_internet_manager.dart';
+import 'package:tictactoe_multiplayer_game/core/network_result.dart';
+
+class InternetManager extends IInternetManager {
+  late final InternetConnectionChecker _connectionChecker;
+  StreamSubscription<InternetConnectionStatus>? _subscription;
+
+  InternetManager() {
+    _connectionChecker = InternetConnectionChecker.createInstance(
+      checkInterval: const Duration(seconds: 3),
+    );
+  }
+
+  @override
+  void handleNetworkChange(NetworkCallback onChange) {
+    _subscription = _connectionChecker.onStatusChange.listen((event) {
+      print('internet : $event');
+      onChange.call(
+        NetworkResult.checkConnectivityResult(event),
+      );
+    });
+  }
+
+  @override
+  void dispose() {
+    _subscription?.cancel();
+  }
+}
